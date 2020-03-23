e08cc8e7d5c01484b1a507a5c03ad05b18b3d0b6
Sourakanti Mandal <52707495+Sonu64@users.noreply.github.com>
2020-03-23 11:29:43 +0530

    first_commit_23-03-2020

---
+++ google_code_jam.py
@@ -0,0 +1,47 @@
+def isCapable(s, able):
+    charge = 1
+    damage = 0
+    for step in s:
+        if step == "S":
+            damage += charge
+        if step == "C":
+            charge *= 2
+    
+    if damage <= able:
+        return True
+    else:
+        return False
+
+
+def main(able, b):
+    if isCapable(b, able):
+        return 0
+    swaps = 0
+    beam = [s for s in b]
+    n = len(beam)
+    for i in range(n-1, -1, -1):
+        for j in range(n-1, -1, -1):
+            if beam[j]=="S" and beam[j-1]=="C":
+                temp = beam[j]
+                beam[j] = beam[j-1]
+                beam[j-1] = temp
+                swaps += 1               
+            if isCapable(beam, able):
+                return swaps
+    if isCapable(beam, able) == False:
+        return "IMPOSSIBLE"
+    return swaps
+
+
+if __name__ == "__main__":
+    t = int(input())
+    output = []
+    while t > 0:
+        rec = input().split()
+        o = main(   int(rec[0]), rec[1]    )
+        output.append(o)
+        t -=  1
+    for i in range(len(output)):
+        print("Case #" + str(i+1) + ": " + str(output[i]))
+    
+    
